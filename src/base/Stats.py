7b6ce36774229ccc214ad5719bb466fe560e6715
Austin Hinckley <Hinckley711@live.missouristate.edu>
2017-04-02 18:38:39 -0500

    Support getting stats for just a player or team

--- src/base/Stats.py
+++ src/base/Stats.py
@@ -6,13 +6,20 @@ class Stats:
     The general form for the statistics will be:
         Year
         -> Stat
-            -> Total number for the stat in that year (across all teams and players)
+            -> Total number for the stat in that year
     '''
 
-    def __init__(self, lines, header = None):
+    def __init__(self, lines, header = None, id = None, idIndex = None):
         '''
         Initialize stats object and populate data from lines
         Use header for stat names. If no header given, use first line in lines
+
+        The id is the team or player id string used to filter results
+        The id index is the integer index of the id column used to filter results
+
+        Examples:
+            Stats(lines, None, 'pujolal01', 0) -- Gets stats for Albert Pujols (filter is player ID)
+            Stats(lines, None, 'STL', 3) -- Gets stats for St. Louis (filter is teamID)
         '''
         self._stats = {}
         yearx = 1    # This seems constant across files, but we could parse header if needed
@@ -21,6 +28,9 @@ class Stats:
 
         # Parse lines to get stats by year
         for row in lines[1:]:
+            if id is not None and idIndex is not None and row[idIndex] != id:
+                continue    # Skip rows that don't match the given ID
+
             # Add year to dict if needed
             year = int(row[yearx])
             if year not in self._stats:
