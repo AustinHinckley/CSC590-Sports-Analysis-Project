2db5889877ce6f1d98b00bf0700ec4e23d116ecb
James Hibben <jmhibben@gmail.com>
2017-05-09 14:13:09 -0500

    Modified Team class file to break apart long lines

--- src/stats/Team.py
+++ src/stats/Team.py
@@ -3,19 +3,38 @@ Contains code for
 '''
 
 # Code for generic relative imports
-import os, sys, inspect
+import os
+import sys
+import inspect
 # realpath() will make your script run, even if you symlink it :)
-cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+cmd_folder = os.path.realpath(
+    os.path.abspath(
+        os.path.split(
+            inspect.getfile(
+                inspect.currentframe()
+            )
+        )[0])
+    )
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
 
 # use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"subfolder")))
+cmd_subfolder = os.path.realpath(
+    os.path.abspath(
+        os.path.join(
+            os.path.split(
+                inspect.getfile(
+                    inspect.currentframe()
+                )
+            )[0], "subfolder")
+        )
+    )
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
 # Info:
-# cmd_folder = os.path.dirname(os.path.abspath(__file__)) # DO NOT USE __file__ !!!
+# cmd_folder = os.path.dirname(os.path.abspath(__file__))
+    # DO NOT USE __file__ !!!
 # __file__ fails if script is called in different ways on Windows
 # __file__ fails if someone does os.chdir() before
 # sys.argv[0] also fails because it doesn't not always contains the path
@@ -23,6 +42,7 @@ if cmd_subfolder not in sys.path:
 # End relative imports
 
 from base.Base import Base
+
 
 class Team(Base):
 
@@ -40,7 +60,7 @@ class Team(Base):
             # convert it to a list
             year = [] + year
         for yr in year:
-            ans += [this._stats.getStatFromYear(year[yr], stat)]
+            ans += [self._stats.getStatFromYear(year[yr], stat)]
         if len(ans) == 1:
             return ans[0]
         return ans
@@ -53,4 +73,4 @@ class Team(Base):
 
 team = Team('tst')
 # team.stats = 'test'
-print(team.stats)
+print((team.stats))
