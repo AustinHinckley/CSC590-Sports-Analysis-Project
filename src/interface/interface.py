38438837011994c5b3a8e053f70fc4710f20155d
Austin Hinckley <Hinckley711@live.missouristate.edu>
2017-05-12 16:26:55 -0500

    Get window to display

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -1,44 +1,40 @@
-import Tkinter as Tk
+import tkinter as Tk
 import matplotlib.pyplot as plt
-from ipywidgets import widgets
 from IPython.display import display
 
 # Putting these here because we might need them
 import matplotlib
-matplotlib.user('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#matplotlib.user('TkAgg')
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class Interface:
     ''' This class will contain the Tkinter objects and various UI-related methods '''
-    WINDOW_TITLE = "Baseball Plotmaster 3000"    # Tentative for sure
+    WINDOW_TITLE = "Baseball Plot GUI"
 
-    def __init__(self, minWidth, minHeight):
+    def __init__(self, minWidth=800, minHeight=600):
         ''' Initialize window and user widgets '''
         self.window = Tk.Tk()
-        window.minsize(width=minWidth, height=minHeight)
-        window.wm_title(WINDOW_TITLE)
+        self.window.minsize(width=minWidth, height=minHeight)
+        self.window.wm_title(Interface.WINDOW_TITLE)
         self._createWidgets()
 
-    def __init__(self):
-        self(600, 400)
-
-    def _createWidgets():
+    def _createWidgets(self):
         ''' Loads all of the user-interaction widgets into the window '''
         pass
 
     # Should somehow include event listeners? Maybe?
 
-    def createPlot(dataX, dataY, markerStyle, markerColor):
+    def createPlot(self, dataX, dataY, markerStyle, markerColor):
         ''' Builds a plot from the given data and adds it to the window '''
         # Can maybe use canvas = FigureCanvasTkAgg(<Figure>, master=window)
         # Then do something like canvas.get_tk_widget().pack(...) or canvas._tkcanvas.pack(...)
         pass
 
-    def display():
+    def display(self):
         ''' Calls mainloop() to show the window '''
         self.window.mainloop()
 
-    def close():
+    def close(self):
         ''' Closes the interface window and destroys it '''
         self.window.quit()
         self.window.destroy()
--- src/main.py
+++ src/main.py
@@ -4,12 +4,17 @@
 from readers.Read import *
 from stats.base.Base import *
 from stats.base.Stats import *
+from interface.interface import *
 
 def main():
-    lines = read("Batting.csv")
+    # Initialize interface
+    UI = Interface()
+    UI.display()
+
+    '''lines = read("Batting.csv")
     stats = Stats(lines, id='pujolal01', idIndex=0)
     print(stats.getStatForAllYears('HR'))
-    print(stats.getStatFromYear(2014, 'HR'))
+    print(stats.getStatFromYear(2014, 'HR'))'''
 
 
 if __name__ == '__main__':
