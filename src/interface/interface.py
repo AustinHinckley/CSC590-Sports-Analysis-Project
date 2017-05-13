20911788b08190c33bb37557ab50170e619732a8
Austin Hinckley <Hinckley711@live.missouristate.edu>
2017-05-13 15:51:42 -0500

    Get dropdown values to change correctly (not going to support player data without specifying team -- it complicates things too much)

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -39,7 +39,6 @@ class Interface:
         self.yrsTeamsPlayers = {}    # 'Year' : { 'Team' : [players]}
         self.years = []
         teamSet = set()
-        playerSet = set()
         if lines is not None:
             # Go through all lines and add years, teams, and players
             for row in lines[1:]:
@@ -58,37 +57,40 @@ class Interface:
                 if player not in self.yrsTeamsPlayers[year][team]:
                     self.yrsTeamsPlayers[year][team].append(player)
 
-                # Add to the initial team and players lists
+                # Add to the team set to get unique teams
                 teamSet.add(team)
-                playerSet.add(player)
 
         # Get sorted list of teams and players
         self.teams = ['All'] + sorted(teamSet)
-        self.players = ['All'] + sorted(playerSet)
+        self.players = ['All']
 
 
     def _createWidgets(self):
-        ''' Initializes widgets in the root window '''
+        ''' Initializes widgets in the root window (with listeners to _changeYearTeamPlayer) '''
         # Start year drop down
         self.startYearVar = StringVar(self.root)
         self.startYearVar.set(self.years[0])
-        self.startYearMenu = OptionMenu(self.root, self.startYearVar, *self.years,
-                                command=self._changeYearTeamPlayer)
+        self.startYearMenu = OptionMenu(self.root, self.startYearVar, *self.years)
+        self.startYearVar.trace('w', self._changeYearTeamPlayer)
+
         # End year drop down
         self.endYearVar = StringVar(self.root)
         self.endYearVar.set(self.years[-1])
-        self.endYearMenu = OptionMenu(self.root, self.endYearVar, *self.years,
-                                        command=self._changeYearTeamPlayer)
+        self.endYearMenu = OptionMenu(self.root, self.endYearVar, *self.years)
+        self.endYearVar.trace('w', self._changeYearTeamPlayer)
+
         # Teams drop down
         self.teamVar = StringVar(self.root)
         self.teamVar.set(self.teams[0])
-        self.teamMenu = OptionMenu(self.root, self.teamVar, *self.teams,
-                                    command=self._changeYearTeamPlayer)
+        self.teamMenu = OptionMenu(self.root, self.teamVar, *self.teams)
+        self.teamVar.trace('w', self._changeYearTeamPlayer)
+
         # Players drop down
         self.playerVar = StringVar(self.root)
         self.playerVar.set(self.players[0])
-        self.playerMenu = OptionMenu(self.root, self.playerVar, *self.players,
-                                        command=self._changeYearTeamPlayer)
+        self.playerMenu = OptionMenu(self.root, self.playerVar, *self.players)
+        self.playerVar.trace('w', self._changeYearTeamPlayer)
+
 
     def _loadWidgets(self):
         ''' Loads widgets into the root window to later be displayed with mainloop '''
@@ -101,17 +103,17 @@ class Interface:
         self.endYearMenu.grid(row=1, column=4)
 
         # Team
-        Label(self.root, text='Team').grid(row=1, column=6)
-        self.teamMenu.grid(row=1, column=7)
+        Label(self.root, text='Team').grid(row=1, column=5)
+        self.teamMenu.grid(row=1, column=6)
 
         # Players
-        Label(self.root, text='Player').grid(row=1, column=9)
-        self.playerMenu.grid(row=1, column=10)
+        Label(self.root, text='Player').grid(row=1, column=7)
+        self.playerMenu.grid(row=1, column=8)
 
 
     ''' Event listeners and operations for the option menus '''
 
-    def _changeYearTeamPlayer(self, value):
+    def _changeYearTeamPlayer(self, *args):
         ''' A value in a dropdown was changed, so update dropdown values as needed '''
         startYear = int(self.startYearVar.get())
         endYear = int(self.endYearVar.get())
@@ -123,24 +125,47 @@ class Interface:
         else:
             self._removeError()
 
-            # Get new years, teams, and players
-            self.years = []
+            # Get teams and players that existed in the date range
             teamSet = set()
             playerSet = set()
             for yrNum in range(startYear, endYear + 1):    # For each year in the given range
                 year = str(yrNum)
-                self.years.append(year)
                 for tm in self.yrsTeamsPlayers[year]:    # For all teams for those years
-                    if teamChosen == 'All' or tm == teamChosen:
-                        # Only add selected team unless 'All' is selected
-                        teamSet.add(tm)
+                    teamSet.add(tm)    # Always add all teams
+                    if teamChosen != 'All' and teamChosen == tm:
+                        # Only add players on the chosen team
+                        # If 'All' is selected for team, don't add any players
                         for plr in self.yrsTeamsPlayers[year][tm]:
-                            # For each player on the team in this year, only add selected
-                            # unless 'All' is selected
-                            if playerChosen == 'All' or plr == playerChosen:
-                                playerSet.add(plr)
+                            playerSet.add(plr)
             self.teams = ['All'] + sorted(teamSet)
             self.players = ['All'] + sorted(playerSet)
+            print(len(self.players), "players")
+
+            # Get list of valid years for the selected team/playerSet
+            self.years = []
+            if teamChosen == 'All' and playerChosen == 'All':
+                # Show all years in the dropdown if 'All' chosen for everything
+                self.years = list(self.yrsTeamsPlayers.keys())
+            elif teamChosen == 'All':
+                # Just player chosen, so do nothing because we aren't going to
+                # support player data without having a given team
+                pass
+            elif playerChosen == 'All':
+                # Just the team chosen, so show all active years for that team
+                for yr in self.yrsTeamsPlayers:
+                    if teamChosen in self.yrsTeamsPlayers[yr]:
+                        self.years.append(yr)
+            else:
+                # Both team and player chosen, so just show all years the player
+                # played on that team
+                for yr in self.yrsTeamsPlayers:
+                    if teamChosen in self.yrsTeamsPlayers[yr] \
+                        and playerChosen in self.yrsTeamsPlayers[yr][teamChosen]:
+                        self.years.append(yr)
+
+            # Make sure something is in years
+            if len(self.years) == 0:
+                self.years = list(self.yrsTeamsPlayers.keys())
 
             # Remove old lists from option menus
             self.startYearMenu['menu'].delete(0, 'end')
@@ -157,6 +182,17 @@ class Interface:
             for plr in self.players:
                 self.playerMenu['menu'].add_command(label=plr, command=lambda v=plr: self.playerVar.set(v))
 
+            # Check for value conflicts and notify the user if necessary
+            if startYear < int(self.years[0]):
+                print(self.years)
+                self._displayError('Please change start year')
+            elif endYear > int(self.years[-1]):
+                self._displayError('Please change end year')
+            elif teamChosen not in self.teams:
+                self._displayError('Please change team selection')
+            elif playerChosen not in self.players:
+                self._displayError('Please change player selection')
+
 
     ''' Plot/display/misc. functions '''
 
@@ -164,6 +200,7 @@ class Interface:
         ''' Shows error message underneath the widgets '''
         self.error = True
         self.errorLabel = Label(self.root, text=text, fg='red')
+        self.errorLabel.grid(row=2, column=1, columnspan=10, sticky=W)
 
     def _removeError(self):
         ''' Removes error message from the grid '''
@@ -174,7 +211,6 @@ class Interface:
         # Can maybe use canvas = FigureCanvasTkAgg(<Figure>, master=window)
         # Then do something like canvas.get_tk_widget().pack(...) or canvas._tkcanvas.pack(...)
         pass
-
 
     def display(self):
         ''' Calls mainloop() to show the window '''
