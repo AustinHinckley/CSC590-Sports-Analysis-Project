be422844ace192a9a97dba125288bdfb6d56ff93
Darren Williams <darrenwilliams@127.0.0.1>
2017-05-14 15:31:58 -0500

    Copy in Jame’s code from the Team class

--- src/stats/Player.py
+++ src/stats/Player.py
@@ -1,15 +1,33 @@
-from base.Base import *
+from base import *
+
+'''
+note: I copied Jame's code from the Team class and
+changed the class name since the functionallity is
+basically the same.
+'''
 
 class Player(Base):
-    '''
-    All data for a player is determined here.
-    '''
-    def __init__(self):
-        Base.__init__()
 
-    @property
-    def years(self):
-        pass
+    def __init__(self, _id):
+        super().__init__(_id)
 
-    @years.setter
-    def years(self, years)
+    def getStatFromYearRange(self, stat, years):
+        '''Get a stat from a particular year or set of years
+           If year is a list of years, it will iterate through
+            the entire list
+           Returns List of Strings, even if there is only one element
+        '''
+        ans = []
+        if isinstance(years, int):
+            # convert it to a list
+            years = list(years)
+        for yr in years:
+            # dev guard to prevent unnecessary TypeErrors
+            if self._stats is not None:
+                ans.append(self._stats.getStatFromYear(years[yr], stat))
+        return ans
+
+    def getStatFromAllYears(self, stat):
+        '''Get a list containing the given stat for all years'''
+        return self.getStats(stat, self._stats.getYearsActive())
+        
